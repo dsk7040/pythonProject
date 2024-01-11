99f971673075243fb23f4dea391bbf4c625977c5
Deepak Soni <132355425+dsk7040@users.noreply.github.com>
2024-01-11 18:02:22 +0530

    file handling is commit

---
+++ methods/filehandling.py
@@ -0,0 +1,27 @@
+import os
+class test():
+    def writefile(self, file, write):
+        self.file = file
+        self.text = write
+        self.file = open(self.file, "w")
+        self.file.write(self.text)
+        self.file.close()
+    def readfile(self, change):
+        self.c = change
+        self.f = open(self.c, 'r')
+        print(self.f.read())
+        self.f.close()
+    def removefile(self, remove):
+        self.r = remove
+        if os.path.exists(self.r):
+            os.remove(self.r)
+            print("File Removed successfully")
+        else:
+            print("The file does not exist")
+
+obj = test()
+obj.writefile("newfile2.txt", "lorem ipsum testing by deepak")
+obj.readfile("newfile2.txt")
+os.rename("demo.py", "filehandling.py") #please check the file path then excute the code
+obj.removefile("newfile2.txt") #first comment rename file then remove our file
+
